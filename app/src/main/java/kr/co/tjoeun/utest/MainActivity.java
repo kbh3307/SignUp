55d2389492bf30594cace5ba10cc02f5487cba4f
kbh3307 <61855314+kbh3307@users.noreply.github.com>
2020-03-17 12:09:51 +0900

    비번 찾기로 버튼 문구 변경

--- app/src/main/java/kr/co/tjoeun/utest/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/utest/MainActivity.java
@@ -3,12 +3,29 @@ package kr.co.tjoeun.utest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button loginBtn = null;
+    TextView findPwTxt = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginBtn = findViewById(R.id.loginBtn);
+        findPwTxt = findViewById(R.id.findPwTxt);
+
+//        로그인버튼의 글자를 => 회원가입으로 바꾸자
+        loginBtn.setText("회원가입");
+
+//        비밀번호 찾기 텍스트뷰를 => JAVA에서 "비번 찾기"로 변경
+//        findPwTxt 로 id를 부여하자
+        findPwTxt.setText("비번 찾기");
     }
 }
